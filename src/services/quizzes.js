23a8e3ad046aa396ffb4b8dc5d43b4a4e4cc836d
Jesper Kuutti <jesper.kuutti@helsinki.fi>
2019-09-13 13:02:01 +0300

    Points visualization has correct max points for all

--- src/services/quizzes.js
+++ src/services/quizzes.js
@@ -13,10 +13,10 @@ export async function fetchQuizzesProgress() {
     `https://quizzes.mooc.fi/api/v1/courses/${id}/users/current/progress`,
     { headers: { Authorization: `Bearer ${accessToken()}` } },
   )
-  let decreaseMaxPoints = 1
+  let decreaseMaxPoints = 3
   const { group } = await fetchAbGroup("self_evaluation_k19_tikape")
   if (group === 3) {
-    decreaseMaxPoints = 0
+    decreaseMaxPoints = 2
   }
   console.log("ab group", group)
   let progress = response.data?.points_by_group
